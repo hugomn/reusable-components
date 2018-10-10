3f931b327bcc0393ef5e5a56ae46a8ba8314190c
Hugo Nogueira <hugomn@gmail.com>
2018-10-10 17:27:50 +0200

    Hello world

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,7 @@
-'use strict';
-
-function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
-
-var React = _interopDefault(require('react'));
+import React from 'react';
 
 var Header = function Header() {
-  return React.createElement("h1", null, "Hello, React Day!");
+  return React.createElement("h1", null, "Hello, world!");
 };
 
-module.exports = Header;
+export default Header;
--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = function() {
-  return <h1>Hello, React Day!</h1>;
+  return <h1>Hello, world!</h1>;
 };
 
 export default Header;
